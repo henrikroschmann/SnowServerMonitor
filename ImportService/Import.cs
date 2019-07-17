9949354e2e03f3869a8cb4dfe7dca6052b30e72a
Henrik <henrik.roschmann@snowsoftware.com>
2019-07-17 13:24:04 +0200

    Added the first APi endpoint and the db import

---
+++ ImportService/Import.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportService
+{
+    public class Import
+    {
+        public static void Process(HelperLibrary.Models.ServerLog[] data)
+        {
+            Database.InsertDataSet(HelperLibrary.Tools.ConvertToDataTable(data.ToList()));          
+                
+        }       
+    }
+}
