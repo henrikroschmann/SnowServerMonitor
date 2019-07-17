6acc6ac177675d98aab473ae2f1fbd35c9c0e8d3
Henrik <henrik.roschmann@snowsoftware.com>
2019-07-17 11:24:35 +0200

    Added the csv import service Added Dapper DB insert and read Started the Node frontend project.

--- HelperLibrary/Tools.cs
+++ HelperLibrary/Tools.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Configuration;
+using System.Data;
+using System.ComponentModel;
 
 namespace HelperLibrary
 {
@@ -10,6 +12,23 @@ namespace HelperLibrary
         public static string GetConnectionString(string name = "DapperDB")
         {
             return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public static DataTable ConvertToDataTable<T>(IList<T> data)
+        {
+            PropertyDescriptorCollection properties =
+               TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
         }
     }
 }
