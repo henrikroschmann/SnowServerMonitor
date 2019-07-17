2f11f26296f200e43d9411246e911d0bbfd4f6cf
Henrik <henrik.roschmann@snowsoftware.com>
2019-07-17 16:19:07 +0200

    WIP

--- ReactFrontEnd/Startup.cs
+++ ReactFrontEnd/Startup.cs
@@ -1,19 +1,19 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ServerMonitorAPI
+namespace ReactFrontEnd
 {
     public class Startup
     {
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }        
+        }
 
         public IConfiguration Configuration { get; }
 
@@ -22,11 +22,11 @@ namespace ServerMonitorAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
             });
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
